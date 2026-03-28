f65cbd82dddf77ac384d7d23622f0f7be32efec4
ZhiYan24 <zhiyanh5@gmail.com>
2026-03-28 08:32:12 -0400

    Added introduction to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,9 @@ def main():
     name = "Zhiyan" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi everyone! My name is Zhiyan and I'm a sophomore from New York. " \
+    "I love logic puzzles, especially situational puzzles and epistemic logic puzzles. " \
+    "I also like to read and play video games in my free time.")
 
 if __name__ == "__main__":
     main()
